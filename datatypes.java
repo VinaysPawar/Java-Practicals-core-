2047cc776e1769d5b15be55e3751f4bfbc44a206
VinaysPawar <119752539+VinaysPawar@users.noreply.github.com>
2022-12-03 23:20:48 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,45 @@
+package basic;
+
+public class datatypes {
+
+	public static void main(String[] args) {
+		boolean IoT = true;
+		 System.out.println(IoT);
+
+		 //byte ex
+		 byte range;
+		 range = 127;
+		 System.out.println(range);
+
+		 //short ex
+		 short range1;
+		 range1 = -1207;
+		 System.out.println(range1);
+
+		 //int ex
+		 int range2 = 3000;
+		 System.out.println(range2);
+
+		 //long ex
+		 long range3 = 3000000L;
+		 System.out.println(range3);
+		//double ex
+		 double number = 99.9;
+		 System.out.println(number);
+
+		 //float ex
+		 float number1 = 99.9f;
+		 System.out.println(number1);
+
+		 //char ex
+		 char letter = '\u0041';
+		 System.out.println(letter);
+		 char letter2 = 65; 
+		 System.out.println(letter2);
+		 char letter3 = 'A';
+		 System.out.println(letter3);
+		// TODO Auto-generated method stub
+
+	}
+
+}
